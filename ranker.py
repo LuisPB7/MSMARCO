65b3a7eb1666b2eb725cfaedcea7da0e5ddb42c9
Luís Pedro Borges <luispb7@hotmail.com>
2020-02-20 19:24:37 +0000

    Update ranker.py

--- ranker.py
+++ ranker.py
@@ -13,7 +13,7 @@ torch.backends.cudnn.benchmark=False
 
 #############################################
 
-K = 1000
+K = 1000 # Re-rank top K
 
 with open("data/passages.pkl", 'rb') as handle:
     passages = pickle.load(handle)
@@ -32,7 +32,7 @@ class PadSequence:
 
         return qids, pids, queries_and_pos_ids, queries_and_pos_attn_masks, queries_and_pos_token_types
 
-class LuceneDataset(Dataset):
+class TestDataset(Dataset):
 
     def __init__(self, lucene_candidates):
         super(LuceneDataset, self).__init__()
@@ -60,7 +60,7 @@ model = model.eval()
 with open("data/top1000.dev.dai.ranks.pkl", 'rb') as handle:
     top1000_dev = pickle.load(handle)
 
-topK_dev_sorted_ranks = {}
+topK_dev_sorted_ranks = {} # Will contain qid:[pid1, pid2, etc] sorted in descending order of score
 topK_dev_sorted_scores = {}
 
 topK_dev = {}
@@ -72,7 +72,7 @@ with torch.no_grad():
     with open('top{}.dev.sorted.tsv'.format(K), 'wt', encoding='utf-8') as out_file:
         tsv_writer = csv.writer(out_file, delimiter=' ')
         query_dict = {qid:{} for qid in topK_dev}
-        data = DataLoader(LuceneDataset(topK_dev), shuffle=True, num_workers=8, batch_size=256, collate_fn=PadSequence())
+        data = DataLoader(TestDataset(topK_dev), shuffle=True, num_workers=8, batch_size=256, collate_fn=PadSequence())
         for batch in data:
             qids = batch[0]
             pids = batch[1]
